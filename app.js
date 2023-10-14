839f9f6129287c93d44b05d44ee3256b7c237aa4
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-10-14 15:35:04 +0300

    Modify the response message

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.get("/*", (req, res) => {
     res.json({
-        message: `Welcome to our api`,
+        message: `Welcome to our Containerized node app`,
         url: req.url,
     });
 });
